ebb90cd7673102cbbacc5d3647400c3e30267a1e
Arslan <arslanali2194@gmail.com>
2018-04-24 23:30:32 +0500

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 50}
-    validates :discription, presence: true, length: { minimum: 15, maximum: 350}
+    validates :description, presence: true, length: { minimum: 15, maximum: 350}
 end 

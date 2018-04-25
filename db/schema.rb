d51c4e4af0dcb2343eb47866d801c2c2f605543e
Arslan <arslanali2194@gmail.com>
2018-04-26 01:14:56 +0500

    update user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425192251) do
+ActiveRecord::Schema.define(version: 20180425200018) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20180425192251) do
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
